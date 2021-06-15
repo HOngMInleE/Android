89f6e5a15a7ec9b1e4943b9c39ff7575fa08af09
hmini0415@naver.com <mini0415*>
2021-06-15 11:51:00 +0900

    0615 3교시

--- 0615SampleIntent/app/src/main/java/org/techtown/a0615sampleintent/MenuActivity.java
+++ 0615SampleIntent/app/src/main/java/org/techtown/a0615sampleintent/MenuActivity.java
@@ -1,11 +1,11 @@
 package org.techtown.a0615sampleintent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -24,6 +24,8 @@ public class MenuActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
 
 
     }
