a2d255ca6a62bd00395213d647529d100c8008be
ori's homie <hmini0415@naver.com>
2021-06-28 16:10:05 +0900

    0628 수업 마무리

--- 0625Receive/app/src/main/java/org/techtown/receive/SMSReceiver.java
+++ 0625Receive/app/src/main/java/org/techtown/receive/SMSReceiver.java
@@ -8,12 +8,14 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SMSReceiver extends BroadcastReceiver {
 
     private static final String TAG = "SmsReceiver";
 
+    public SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -31,6 +33,8 @@ public class SMSReceiver extends BroadcastReceiver {
 
             Date receivedDate = new Date(messages[0].getTimestampMillis());
             Log.i(TAG, "SMS received date : " + receivedDate.toString());
+
+            sendToActivity(context, sender, contents, receivedDate);
         }
     }
 
@@ -53,6 +57,20 @@ public class SMSReceiver extends BroadcastReceiver {
         return messages;
     }
 
+    private void sendToActivity(Context context, String sender, String contents, Date receivedDate) {
+        Intent myIntent = new Intent(context, SmsActivity.class);
+
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|
+                Intent.FLAG_ACTIVITY_SINGLE_TOP|
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        myIntent.putExtra("sender", sender);
+        myIntent.putExtra("contents", contents);
+        myIntent.putExtra("receivedDate", format.format(receivedDate) );
+
+
+        context.startActivity(myIntent);
+    }
 
 
 
