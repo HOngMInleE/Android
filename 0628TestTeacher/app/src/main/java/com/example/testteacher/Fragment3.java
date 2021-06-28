a5ba929293d436c776b918f18d49b2bfceeef3c1
ori's homie <hmini0415@naver.com>
2021-06-28 14:54:01 +0900

    0628 with Teacher

---
+++ 0628TestTeacher/app/src/main/java/com/example/testteacher/Fragment3.java
@@ -0,0 +1,35 @@
+package com.example.testteacher;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+
+
+public class Fragment3 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_3, container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onGotoMainFragment();
+
+            }
+        });
+
+        return rootView;
+    }
+
+}
---
+++ 0628TestTeacher/app/src/main/java/com/example/testteacher/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.testteacher;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    Button button1;
+    Button button2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = (Fragment1) getSupportFragmentManager().findFragmentById(R.id.container);
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        button1.findViewById(R.id.button1);
+        button2.findViewById(R.id.button2);
+
+    }
+
+    public void onButton1(View view) {
+//        button1.setTypeface(Typeface.create("", Typeface.BOLD));
+//        button1.setTextColor(Color.RED);
+//        button2.setTypeface(Typeface.create("", Typeface.NORMAL));
+//        button2.setTextColor(Color.BLACK);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+    }
+
+    public void onButton2(View view) {
+//        button1.setTypeface(Typeface.create("", Typeface.NORMAL));
+//        button1.setTextColor(Color.BLACK);
+//        button2.setTypeface(Typeface.create("", Typeface.BOLD));
+//        button2.setTextColor(Color.RED);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+    }
+
+
+    public void onGotoMainFragment() {
+//        button1.setTypeface(Typeface.create("", Typeface.NORMAL));
+//        button1.setTextColor(Color.BLACK);
+//        button2.setTypeface(Typeface.create("", Typeface.NORMAL));
+//        button2.setTextColor(Color.BLACK);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+
+    }
+
+
+}
