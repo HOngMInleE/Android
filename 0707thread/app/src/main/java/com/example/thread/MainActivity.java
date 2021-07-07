ef1b96b6da56f80e5c6d02150f58f77b6993e0d4
ori's homie <hmini0415@naver.com>
2021-07-07 17:34:45 +0900

    0707 Thread Test fin

--- 0707thread/app/src/main/java/com/example/thread/MainActivity.java
+++ 0707thread/app/src/main/java/com/example/thread/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.thread;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
 
-    MainHandler handler;
+//    MainHandler handler;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +28,14 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
+             @Override
             public void onClick(View v) {
                 BackGroundThread thread  = new BackGroundThread();
                 thread.start();
             }
         });
 
-        handler = new MainHandler();
+//        handler = new MainHandler();
     }
 
     class BackGroundThread extends Thread {
@@ -51,26 +52,37 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Thread", "value" + value);
 //                textView.setText(value); // 프로그램 강제 다운 됨. // GUI, View 들에 대한 데이터 처리는 Main Thread에서만 가능.
 
-                Message message = handler.obtainMessage();
-                Bundle bundle = new Bundle();
-                bundle.putInt("value", value);
-                message.setData(bundle);
+//                Message message = handler.obtainMessage();
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("value", value);
+//                message.setData(bundle);
+//
+//                handler.sendMessage(message);
 
-                handler.sendMessage(message);
+    // Runnable 객체 실행 방식
+
+                handler.post(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText(String.valueOf(value));
+                            }
+                        }
+                );
             }
         }
     }
 
-    class MainHandler extends Handler {
-        @Override
-        public void handleMessage( Message msg) {
-            super.handleMessage(msg);
-
-            Bundle bundle = msg.getData();
-            int value = bundle.getInt("value");
-//            textView.setText("value 값 : " + value);
-            textView.setText(String.valueOf(value));
-        }
-    }
+//    class MainHandler extends Handler {
+//        @Override
+//        public void handleMessage( Message msg) {
+//            super.handleMessage(msg);
+//
+//            Bundle bundle = msg.getData();
+//            int value = bundle.getInt("value");
+////            textView.setText("value 값 : " + value);
+//            textView.setText(String.valueOf(value));
+//        }
+//    }
 
 }
