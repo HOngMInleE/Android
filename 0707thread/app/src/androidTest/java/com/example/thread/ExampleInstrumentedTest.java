23790c66b1e4a9bd53a23d23fcbf3d33927d791d
ori's homie <hmini0415@naver.com>
2021-07-07 17:15:59 +0900

    0707 Thread Test

--- 0707thread/app/src/androidTest/java/com/example/thread/ExampleInstrumentedTest.java
+++ 0707thread/app/src/androidTest/java/com/example/thread/ExampleInstrumentedTest.java
@@ -1,9 +1,9 @@
-package com.example.mission15;
+package com.example.thread;
 
 import android.content.Context;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.mission15", appContext.getPackageName());
+        assertEquals("com.example.thread", appContext.getPackageName());
     }
 }
---
+++ 0707thread/app/src/main/java/com/example/thread/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.thread;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+
+    MainHandler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackGroundThread thread  = new BackGroundThread();
+                thread.start();
+            }
+        });
+
+        handler = new MainHandler();
+    }
+
+    class BackGroundThread extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i=0; i<100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e ) {
+
+                }
+                value += 1;
+                Log.d("Thread", "value" + value);
+//                textView.setText(value); // 프로그램 강제 다운 됨. // GUI, View 들에 대한 데이터 처리는 Main Thread에서만 가능.
+
+                Message message = handler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("value", value);
+                message.setData(bundle);
+
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    class MainHandler extends Handler {
+        @Override
+        public void handleMessage( Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            int value = bundle.getInt("value");
+//            textView.setText("value 값 : " + value);
+            textView.setText(String.valueOf(value));
+        }
+    }
+
+}
